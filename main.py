dd47d69e55e4617ac9f07cb3ae4682a206cc7af7
sulemansarwarr <sulemansarwarr1@gmail.com>
2025-08-01 23:30:04 +0500

    add proxies

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
 from fake_useragent import UserAgent
 import random
+
+# to get proxies from website
+try:
+    df = pd.read_html("https://free-proxy-list.net/en/")
+    ips = df[0]["IP Address"].tolist()
+    proxies = ips[1:50]
+except Exception as e:
+    print("Error fetching proxy list:", e)
+    proxies = []
+
+try:
+    ua = UserAgent()
+    user_agents = ua.random
+except:
+    user_agents = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)..."
+
+# search product
 search= input("Enter the product name for search ")
 search_url=f"https://www.daraz.pk/catalog/?spm=a2a0e.tm80335142.search.d_go&q={search}"
+page_count = 0
 next_page = 2
 
-ua = UserAgent()
-# user_agent = ua.random
-try:
-    user_agent = ua.random
-except:
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)..."
-
-# Setup Firefox options with fake user-agent
-firefox_options = Options()
-firefox_options.set_preference("general.useragent.override", user_agent)
-# firefox_options.add_argument("--headless")  # optional: remove if you want to see browser
-
-# Initialize WebDriver
-service = Service()
-driver = webdriver.Firefox(service=service, options=firefox_options)
-# firefox_options = Options()
-# service = Service()  # You can also do Service("path/to/chromedriver")
-# driver = webdriver.Firefox(service=service, options=firefox_options)
-
+# creating lists
 mall_product=[]
 name_of_product=[]
 price_of_product=[]
@@ -37,20 +38,53 @@ soldout_product=[]
 location_of_product=[]
 rating_of_product=[]
 
+# for using random proxies
+proxy = random.choice(proxies)
+user_agent = random.choice(user_agents)
+firefox_options = Options()
+firefox_options.add_argument(f'--proxy-server=http://{proxy}')
+# firefox_options.set_preference("general.useragent.override", user_agent)
+# firefox_options.add_argument("--headless")  # optional
+service = Service()
+driver = webdriver.Firefox(service=service, options=firefox_options)
+
+# def routate_proxies(proxies):
+
 
 while search_url:
     try:
-        
         driver.get(search_url)
         time.sleep(random.uniform(4, 6)) 
-
         html = driver.page_source
         if "captcha" in html.lower():
-            print("Captcha detected, stopping")
-            break
-
-        #driver.quit()
-
+            print("Captcha detected, changing proxy")
+            # if proxiy fails try this
+            MAX_RETRIES = 50
+            retries = 0
+            while retries < MAX_RETRIES:
+                try:
+                    print(f"[INFO] Trying Page {next_page - 1} with Proxy {proxy}")
+                    driver.set_page_load_timeout(20)  # optional: limits hanging
+                    driver.get(search_url)
+                    time.sleep(random.uniform(4, 6))
+                    break  # If successful, exit retry loop
+                except (TimeoutException, WebDriverException) as e:
+                    print(f"[ERROR] Proxy failed: {proxy}, retrying with new one...")
+                    retries += 1
+                    try:
+                        driver.quit()
+                    except:
+                        pass  # If already closed
+                    
+                    proxy = random.choice(proxies)
+                    user_agent = random.choice(user_agents)
+                    firefox_options = Options()
+                    firefox_options.add_argument(f'--proxy-server=http://{proxy}')
+                    service = Service()
+                    driver = webdriver.Firefox(service=service, options=firefox_options)
+            else:
+                print(f"[FATAL] All {MAX_RETRIES} retries failed. Skipping this page.")
+                pass
         soup = BeautifulSoup(html, 'html.parser')
         container = soup.find('div', class_='_17mcb')
 
@@ -76,7 +110,7 @@ while search_url:
             else:
                 data = "None"
                 print(data)
-                product_name.append(data)
+                name_of_product.append(data)
 
             product_price= product.find('div', class_="aBrP0")
             if product_price:
@@ -86,7 +120,7 @@ while search_url:
             else:
                 data = "None"
                 print(data)
-                product_price.append(data)
+                price_of_product.append(data)
 
             product_best_price= product.find('div', class_="WNoq3")
             if product_best_price:
@@ -117,7 +151,7 @@ while search_url:
             else:
                 data = "None"
                 print(data)
-                product_location.append(data)
+                location_of_product.append(data)
 
             product_rating= product.find('span', class_="qzqFw")
             if product_rating:
@@ -128,15 +162,29 @@ while search_url:
                 data = "None"
                 print(data)
                 rating_of_product.append(data)
+
+        # storing data in csv after each page
+        my_dic={"Product":mall_product,"Name of product":name_of_product,"Price of product":price_of_product,"best price product":best_price_product,"Soldout product":soldout_product,"Location of product":location_of_product,"Rating of product":rating_of_product}
+        df = pd.DataFrame(my_dic)
+        df.to_csv("data.csv", mode='a', index=False , header=False) 
                 
         button = soup.find("div", class_="e5J1n")
         if button:
-            search_url=f"https://www.daraz.pk/catalog/?q=phone&spm=a2a0e.tm80335142.search.d_go&page={next_page}"
+            search_url = f"https://www.daraz.pk/catalog/?q={search}&spm=a2a0e.tm80335142.search.d_go&page={next_page}"
             next_page += 1
+            page_count += 1
         else:
             search_url=None
+        
+        if page_count % 25 == 0 and page_count != 0:
             driver.quit()
-            break
+            print(f"[INFO] Switching Proxy after {page_count} pages")
+            proxy = random.choice(proxies)
+            user_agent = random.choice(user_agents)
+            firefox_options = Options()
+            firefox_options.add_argument(f'--proxy-server=http://{proxy}')
+            service = Service()
+            driver = webdriver.Firefox(service=service, options=firefox_options)
         
         # print(len(mall_product))
         # print(len(name_of_product))
@@ -146,8 +194,10 @@ while search_url:
         # print(len(location_of_product))
         # print(len(rating_of_product))
 
-    except:
-        break
+    except Exception as e:
+        print("Exception occurred:", e)
+
+driver.quit()
     
 my_dic={"Product":mall_product,"Name of product":name_of_product,"Price of product":price_of_product,"best price product":best_price_product,"Soldout product":soldout_product,"Location of product":location_of_product,"Rating of product":rating_of_product}
 df = pd.DataFrame(my_dic)
